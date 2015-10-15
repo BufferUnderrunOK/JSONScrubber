fb1cb5cff29550dfdf3f88ef5c99a52082bbcff4
Barrett Nuzum <barrett.nuzum@gmail.com>
2015-10-15 13:48:01 -0500

    Added source code

---
+++ JSONScrubber.Tests/ScrubberTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace JSONScrubber.Tests
+{
+    [TestClass]
+    public class ScrubberTests
+    {
+        private string _samplePayload = @"{
+  ""wholesaler"":""US Foods"",
+  ""delivered"":""2015-06-19T05:15:00-0500"",
+  ""contacts"": [
+    {
+      ""wholesaler"":""US Foods"",
+      ""name"":""John Lederer""
+    },
+    {
+      ""wholesaler"":""Sysco"",
+      ""name"":""Bill Delaney""
+    }
+  ]
+}";
+        private Scrubber _scrubber;
+
+        [TestInitialize]
+        public void SetUp() 
+        {
+            _scrubber = new Scrubber();
+        }
+
+        [TestMethod]
+        public void ScrubUsingHoeggPayload()
+        {
+            var results = _scrubber.Scrub(_samplePayload);
+
+            Assert.IsNotNull(results);
+            Assert.AreEqual(2, results.contacts.Count);
+            Assert.IsFalse(((IDictionary<string,object>) results.contacts[1]).ContainsKey("wholesaler"));
+        }
+    }
+}
---
+++ JSONScrubber/Scrubber.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONScrubber
+{
+    public class Scrubber 
+    {
+        private IDictionary<string, int> _keyLevels;
+
+        public Scrubber()
+        {
+            _keyLevels = new Dictionary<string, int>();
+        }
+
+        public dynamic Scrub(string _samplePayload)
+        {
+            var converter = new ExpandoObjectConverter();
+            var expando = JsonConvert.DeserializeObject<ExpandoObject>(_samplePayload, converter);
+            var expandoDict = (IDictionary<string, object>)expando;
+
+            PopulateLevels(expandoDict, 1);
+            RemoveNestedRepeatedFields(expandoDict, 1);
+
+            return expando;
+        }
+
+        private void RemoveNestedRepeatedFields(IDictionary<string, object> dict, int level)
+        {
+            foreach (var property in dict.Keys.ToList())
+            {
+                var value = dict[property];
+                if (value is IEnumerable<dynamic>)
+                {
+                    foreach (var entry in (IEnumerable<dynamic>)value)
+                    {
+                        RemoveNestedRepeatedFields((IDictionary<string, object>)entry, level + 1);
+                    }
+                }
+                if (_keyLevels[property] < level)
+                {
+                    dict.Remove(property);
+                }
+            }
+        }
+
+        private void PopulateLevels(IDictionary<string,object> dict, int level)
+        {
+            foreach (var property in dict.Keys.ToList())
+            {
+                var value = dict[property];
+                if (value is IEnumerable<dynamic>)
+                { 
+                    foreach(var entry in (IEnumerable<dynamic>) value) {                       
+                        PopulateLevels(((IDictionary<string, object>) entry), level + 1);
+                    }
+                }
+
+                if (!_keyLevels.ContainsKey(property))
+                {
+                    _keyLevels.Add(property, level);
+                } else if (_keyLevels[property] > level)
+                {
+                    _keyLevels[property] = level;
+                }
+            }
+        }
+    }
+}
